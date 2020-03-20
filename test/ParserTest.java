80e197392ad9d58526a29e64da7cd57ced2e559d
filipasenra <filipasenra4@gmail.com>
2020-03-20 10:39:37 +0000

    Commeting tests

--- test/ParserTest.java
+++ test/ParserTest.java
@@ -63,149 +63,126 @@ public class ParserTest {
     public void testHelloWorld() {
         test("fixtures/public/HelloWorld.jmm", false);
     }
-*/
-/*
+
+
     @Test
     public void testLazysort() {
         test("fixtures/public/Lazysort.jmm", false);
     }
-*/
-/*
+
     @Test
     public void testLife() {
         test("fixtures/public/Life.jmm", false);
     }
-*/
-/*
+
     @Test
     public void testMonteCarloPi() {
         test("fixtures/public/MonteCarloPi.jmm", false);
     }
-*/
-/*
+
     @Test
     public void testQuickSort() {
         test("fixtures/public/QuickSort.jmm", false);
     }
-*/
-/*
+
     @Test
     public void testSimple() {
         test("fixtures/public/Simple.jmm", false);
     }
-*/
-/*
+
     @Test
     public void testTicTacToe() {
         test("fixtures/public/TicTacToe.jmm", false);
     }
-*/
-/*
+
     @Test
     public void testWhileAndIF() {
         test("fixtures/public/WhileAndIF.jmm", false);
     }
-*/
-/*
+
     @Test
     public void testarr_index_not_int() {
         test("fixtures/public/fail/semantic/arr_index_not_int.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testarr_size_not_int() {
         test("fixtures/public/fail/semantic/arr_size_not_int.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testbadArguments() {
         test("fixtures/public/fail/semantic/badArguments.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testbinop_incomp() {
         test("fixtures/public/fail/semantic/binop_incomp.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testfuncNotFound() {
         test("fixtures/public/fail/semantic/funcNotFound.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testsimple_length() {
         test("fixtures/public/fail/semantic/simple_length.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testvar_exp_incomp() {
         test("fixtures/public/fail/semantic/var_exp_incomp.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testvar_lit_incomp() {
         test("fixtures/public/fail/semantic/var_lit_incomp.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testvar_undef() {
         test("fixtures/public/fail/semantic/var_undef.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testvarNotInit() {
         test("fixtures/public/fail/semantic/varNotInit.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testmiss_type() {
         test("fixtures/public/fail/semantic/extra/miss_type.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testBlowUp() {
         test("fixtures/public/fail/syntactical/BlowUp.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testCompleteWhileTest() {
         test("fixtures/public/fail/syntactical/CompleteWhileTest.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testLengthError() {
         test("fixtures/public/fail/syntactical/LengthError.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testMissingRightPar() {
         test("fixtures/public/fail/syntactical/MissingRightPar.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testMultipleSequential() {
         test("fixtures/public/fail/syntactical/MultipleSequential.jmm", true);
     }
-*/
-/*
+
     @Test
     public void testNestedLoop() {
         test("fixtures/public/fail/syntactical/NestedLoop.jmm", true);
     }
-*/
+    */
 }
