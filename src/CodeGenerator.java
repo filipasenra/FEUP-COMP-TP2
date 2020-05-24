32c339c49611e05a98dcbb4e900e6280b9bc2f6c
Cláudia <claudiainescostamartins@gmail.com>
2020-05-24 15:45:36 +0100

    Fixed bug in array initialization

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -792,14 +792,7 @@ public class CodeGenerator {
     }
 
     private void generateArrayInitialization(ASTInitializeArray arrayInit, SymbolClass symbolClass, SymbolMethod symbolMethod) {
-        if (arrayInit.jjtGetChild(0) instanceof ASTLiteral) {
-            ASTLiteral arg = (ASTLiteral) arrayInit.jjtGetChild(0);
-            loadIntLiteral(arg.val);
-        }
-        else if (arrayInit.jjtGetChild(0) instanceof ASTIdentifier) {
-            ASTIdentifier arg = (ASTIdentifier) arrayInit.jjtGetChild(0);
-            loadVariable(arg.val, symbolClass, symbolMethod);
-        }
+        this.generateExpression((SimpleNode) arrayInit.jjtGetChild(0), symbolClass, symbolMethod);
 
         this.bodyCode.append("\tnewarray int\n");
         incrementStack();
